86fd83ab92f873c6ab5cfbe8531cce7aefa223d3
RockChinQ <rockchinq@gmail.com>
2026-07-27 18:32:42 +0800

    docs(docs): add Fumadocs user guide (#25)

---
+++ docs-site/next.config.mjs
@@ -0,0 +1,14 @@
+import { createMDX } from "fumadocs-mdx/next";
+
+const withMDX = createMDX();
+
+/** @type {import("next").NextConfig} */
+const config = {
+  output: "export",
+  basePath: "/docs",
+  trailingSlash: true,
+  reactStrictMode: true,
+  images: { unoptimized: true }
+};
+
+export default withMDX(config);
